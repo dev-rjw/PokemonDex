060dc4904c44fe0d334a0a1d8016838cf5f173fe
dev-rjw <rjw0105@gmail.com>
2024-08-26 10:17:25 +0900

    Dex 로직 코드스니펫 작성

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,5 +1,25 @@
-const Dex = () => {
-    return <></>;
-};
+import { useState } from "react";
+import Dashboard from "../components/Dashboard";
+import PokemonList from "../components/PokemonList";
+import MOCK_DATA from "../mock";
+
+function Dex() {
+    const [selectedPokemon, setSelectedPokemon] = useState([]);
+
+    const addPokemon = (pokemon) => {};
+
+    const removePokemon = (pokemon) => {
+        {
+            /* 제거는 필터를 이용해 봅시다 */
+        }
+    };
+
+    return (
+        <div>
+            <Dashboard selectedPokemon={selectedPokemon} onRemovePokemon={removePokemon} />
+            <PokemonList pokemonList={MOCK_DATA} onAddPokemon={addPokemon} />
+        </div>
+    );
+}
 
 export default Dex;

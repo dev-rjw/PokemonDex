d365f5f87738a6c3ffdcc6565601548a1d0129f0
dev-rjw <rjw0105@gmail.com>
2024-08-26 19:03:05 +0900

    sweetalert2 추가

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
+import Swal from "sweetalert2";
 
 export const DexContext = createContext();
 
@@ -9,12 +10,18 @@ function Dex() {
 
     const addPokemon = (pokemon) => {
         if (selectedPokemon.length >= 6) {
-            alert("포켓몬은 최대 여섯개까지만 선택 할 수 있어요.");
+            Swal.fire({
+                icon: "warning",
+                text: "포켓몬은 최대 여섯개까지만 선택 할 수 있어요.",
+            });
             return;
         }
 
         if (selectedPokemon.filter((element) => element.id === pokemon.id).length !== 0) {
-            alert("이미 선택된 포켓몬입니다.");
+            Swal.fire({
+                icon: "warning",
+                text: "이미 선택된 포켓몬입니다.",
+            });
             return;
         }
 

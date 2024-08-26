7e90279afb29672e837147911321e1a7e0d6b04b
dev-rjw <rjw0105@gmail.com>
2024-08-26 09:32:55 +0900

    PokemonCard, PokemonList 스타일 적용 및 함수 연결

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,46 @@
 import styled from "styled-components";
 
-const Card = styled.div``;
+const Card = styled.div`
+    border: 1px solid rgb(221, 221, 221);
+    background-color: rgb(255, 255, 255);
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 8px;
+    text-align: center;
+    padding: 10px;
+    cursor: pointer;
+    transition: transform 0.2s, box-shadow 0.2s;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+    margin-top: 10px;
+    padding: 5px 10px;
+    font-size: 12px;
+    cursor: pointer;
+    border: none;
+    background-color: rgb(255, 0, 0);
+    color: rgb(255, 255, 255);
+    border-radius: 5px;
+`;
+
+const P1 = styled.p`
+    font-size: 14px;
+    font-weight: bold;
+    margin: 5px 0px;
+    color: black;
+`;
+const P2 = styled.p`
+    font-size: 12px;
+    color: rgb(102, 102, 102);
+`;
 
 function PokemonCard({ pokemon, onAdd, onRemove, isSelected }) {
     return (
         <Card>
-            <img src={""} alt={""} />
-            <p>{pokemon.korean_name}</p>
-            {isSelected ? <Button onClick={() => {}}>삭제</Button> : <Button onClick={() => {}}>추가</Button>}
+            <img src={pokemon.img_url} alt={pokemon.korean_name} />
+            <P1>{pokemon.korean_name}</P1>
+            <P2>No.{String(pokemon.id).padStart(3, "0")}</P2>
+            {isSelected ? <Button onClick={() => onRemove()}>삭제</Button> : <Button onClick={() => onAdd()}>추가</Button>}
         </Card>
     );
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 import PokemonCard from "./PokemonCard";
 
-const ListContainer = styled.div``;
+const ListContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    gap: 20px;
+    background-color: rgb(240, 240, 240);
+    padding: 20px;
+    border: 1px solid rgb(221, 221, 221);
+    border-radius: 10px;
+`;
 
 const PokemonList = ({ pokemonList, onAddPokemon }) => {
     return (
         <ListContainer>
             {pokemonList.map((pokemon) => (
-                <PokemonCard key={pokemon.id} pokemon={{}} onAdd={() => {}} isSelected={false} />
+                <PokemonCard key={pokemon.id} pokemon={pokemon} onAdd={() => onAddPokemon()} isSelected={false} />
             ))}
         </ListContainer>
     );

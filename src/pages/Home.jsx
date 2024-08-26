27da64e18bc74263e347d5a8daf24ac16df04449
dev-rjw <rjw0105@gmail.com>
2024-08-26 09:09:25 +0900

    Home 화면 UI 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+    margin-top: 10px;
+    padding: 10px 20px;
+    font-size: 18px;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: rgb(255, 0, 0);
+    color: white;
+    border: none;
+    transition: background-color 0.3s;
+`;
 
 function Home() {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <h1>포켓몬 도감</h1>
-            <button onClick={() => navigate("/dex")}>포켓몬 도감 시작하기</button>
-        </div>
+        <>
+            <div>
+                <img src="https://react-6-pokemon.vercel.app/assets/pokemon-logo-RN0wntMB.png" width="47%"></img>
+            </div>
+            <Button onClick={() => navigate("/dex")}>포켓몬 도감 시작하기</Button>
+        </>
     );
 }
 
